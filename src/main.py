9dc9caaac827d994469851961ca80e4475b78e45
vladimirmac <vladimirbodrin89@gmail.com>
2024-04-24 14:04:35 +0400

    first_commit

--- src/main.py
+++ src/main.py
@@ -3,14 +3,19 @@ from google.protobuf.json_format import MessageToJson
 
 from grpc_client import UnaryClient
 from src.schemas.auth_schemas import AuthIn, AuthOut
+from src.view.auth_view import auth_router
 
-app = FastAPI()
+app = FastAPI(
+    title="Auth Service",
+    debug=True
+)
 
+app.include_router(auth_router)
 
-@app.post('/auth', response_model=AuthOut)
-async def get_jwt_token(auth_data: AuthIn):
-    auth_data_json = auth_data.json()
-    client = UnaryClient()
-    result = client.get_url(message=auth_data_json)
-    json_response = MessageToJson(message=result, preserving_proto_field_name=True)
-    return AuthOut.parse_raw(json_response)
+# @app.post('/auth', response_model=AuthOut)
+# async def get_jwt_token(auth_data: AuthIn):
+#     auth_data_json = auth_data.json()
+#     client = UnaryClient()
+#     result = client.get_url(message=auth_data_json)
+#     json_response = MessageToJson(message=result, preserving_proto_field_name=True)
+#     return AuthOut.parse_raw(json_response)
---
+++ src/view/auth_view.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+from google.protobuf.json_format import MessageToJson
+
+from grpc_client import UnaryClient
+from src.schemas.auth_schemas import AuthOut, AuthIn
+
+auth_router = APIRouter(prefix="/auth", tags=["auth"])
+
+
+@auth_router.post("/auth", response_model=AuthOut)
+async def generate_jwt_token(auth_data: AuthIn):
+    """View для получения access и refresh токенов"""
+    auth_data_json = auth_data.json()
+    client = UnaryClient()
+    result = client.get_url(message=auth_data_json)
+    json_response = MessageToJson(message=result, preserving_proto_field_name=True)
+    return AuthOut.parse_raw(json_response)
